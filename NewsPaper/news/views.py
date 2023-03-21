6f000339828e4d727295da388e0d7cfd3fb32076
Dmitr-iy <KOSHELD@INBOX.RU>
2023-03-21 23:32:09 +0500

    install commit

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .filters import PostFilter
 from .forms import *
-from .models import Post
+from .models import Post, Author
 
 
 class PostList(ListView):
@@ -51,6 +51,7 @@ class PostDelete(DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('post_list')
+    context_object_name = 'title'
 
 class ArticlesCreate(CreateView):
     form_class = PostForm
