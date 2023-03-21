a7c194857401645cb9244927028bab525f0c4158
Dmitr-iy <KOSHELD@INBOX.RU>
2023-03-21 23:32:09 +0500

    install commit
    (cherry picked from commit 6f000339828e4d727295da388e0d7cfd3fb32076)
    
    # Conflicts:
    #	NewsPaper/db.sqlite3
    #	NewsPaper/news/views.py
    #	NewsPaper/templates/post_delete.html

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,30 +1,27 @@
 from datetime import datetime
-
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .filters import PostFilter
 from .forms import *
-from .models import Post, Category
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from .models import Post
+
 
 class PostList(ListView):
     model = Post
     ordering = 'title'
-    template_name = 'news/heads.html'
+    template_name = 'heads.html'
     context_object_name = 'titles'
     paginate_by = 2
 
 class PostDetail(DetailView):
     model = Post
-    template_name = 'news/head.html'
+    template_name = 'head.html'
     context_object_name = 'title'
 
 
 class PostSearch(ListView):
     model = Post
-    template_name = 'news/search.html'
+    template_name = 'search.html'
     context_object_name = 'search'
     queryset = Post.objects.order_by('-dateTime')
     paginate_by = 10
@@ -36,62 +33,31 @@ class PostSearch(ListView):
         context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
         return context
 
-class NewsCreate(PermissionRequiredMixin, CreateView):
-    permission_required = 'news.add_post'
+class NewsCreate(CreateView):
     form_class = PostForm
     model = Post
-    template_name = 'news/news_create.html'
+    template_name = 'news_create.html'
     def form_valid(self, form):
         post = form.save(commit=False)
         post.quantity = 'News'
         return super().form_valid(form)
 
-class PostUpdate(PermissionRequiredMixin, UpdateView):
-    permission_required = 'news.change_post'
+class PostUpdate(UpdateView):
     form_class = PostForm
     model = Post
-    template_name = 'news/news_create.html'
+    template_name = 'news_create.html'
 
-class PostDelete(PermissionRequiredMixin, DeleteView):
-    permission_required = 'news.delete_post'
+class PostDelete(DeleteView):
     model = Post
-    template_name = 'news/post_delete.html'
+    template_name = 'post_delete.html'
     success_url = reverse_lazy('post_list')
-    context_object_name = 'title'
 
-class ArticlesCreate(PermissionRequiredMixin, CreateView):
-    permission_required = 'news.add_post'
+class ArticlesCreate(CreateView):
     form_class = PostForm
     model = Post
-    template_name = 'news/article_create.html'
+    template_name = 'article_create.html'
     def form_valid(self, form):
         post = form.save(commit=False)
         post.quantity = 'Article'
         return super().form_valid(form)
 
-class CategoryListView(ListView):
-    model = Post
-    ordering = 'post'
-    template_name = 'category_list.html'
-    context_object_name = 'category_news_list'
-
-
-    def get_queryset(self):
-        self.category = get_object_or_404(Category, id = self.kwargs['pk'])
-        queryset = Post.objects.filter(category = self.category).order_by('-dateTime')
-        return queryset
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['is_not_subscriber'] = self.request.user not in self.category.subscribers.all()
-        context['category'] = self.category
-        return context
-
-
-@login_required
-def subscribe(request, pk):
-    user =request.user
-    category = Category.objects.get(id=pk)
-    category.subscribers.add(user)
-    message = 'You have subscribed to the newsletter and articles!'
-    return render(request, 'subscribe.html', {'category': category, 'message': message})
